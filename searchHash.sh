9a0624031776c31f6af2ecaba9d2954fbae2cbc4
fbuechel92 <fabianbuecheler@gmx.de>
2024-12-31 12:25:12 +0100

    Working solution without path provided

--- searchHash.sh
+++ searchHash.sh
@@ -3,3 +3,14 @@
 #Usage:
 #searchHash.sh < HASH (sha1sum)
 
+# First, storing the hash provided
+HASHINPUT="$1"
+
+# Next, identify all file names, hash them and compare with hashinput
+find ./ -type f -maxdepth 1 | while read FILEINPUT
+do
+    HASHOUTPUT=$(shasum -a 1 "$FILEINPUT" | cut -d ' ' -f 1)
+    if [[ $HASHOUTPUT == $HASHINPUT ]] 
+        then echo 'A match has been found!'
+    fi
+done
