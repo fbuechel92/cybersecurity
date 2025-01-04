a129d978f8ef1484ed256af5b49b94779e19a4d5
fbuechel92 <fabianbuecheler@gmx.de>
2025-01-04 20:02:55 +0100

    Change text

--- sumBytes.sh
+++ sumBytes.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/ zsh
 #
 #Usage:
-#zsh sumOccurences.sh < inputfileIpBytes.txt
+#zsh sumBytes.sh < inputfileIpBytes.txt
 
+declare -A dict
 
+while read IP
+do
+    dict[$IP]=$(( $dict[$IP]+1 ))
+    echo "$dict[$IP]"
+done
