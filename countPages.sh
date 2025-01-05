0d19263703d43364dcde37c145f59258abb4c1d0
fbuechel92 <fabianbuecheler@gmx.de>
2025-01-05 14:11:55 +0100

    Finish script

--- countPages.sh
+++ countPages.sh
@@ -1,8 +1,21 @@
 #! /bin/zish
 #
 #Goal:
-#read the access log file, count occurence by page
+#read the access log file, count occurence by page for given parameter ip
 #
 #Usage:
-#zsh countPages.sh < access.log
+#zsh countPages.sh <IP Address> < access.log
 
+declare -A dict
+
+while read addr d1 d2 datim gmtoff getr page therest
+do
+    if [[ $addr == $1 ]]
+        then dict[$page]=$(( $dict[$page]+1 ))
+    fi
+done
+
+for key in ${(@k)dict}
+do
+    echo "$dict[$key]: $key"
+done
