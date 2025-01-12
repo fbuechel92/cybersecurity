8c68515313eabd256de3fdf47a10f438ec854e11
fbuechel92 <fabianbuecheler@gmx.de>
2025-01-12 20:09:29 +0100

    Read local IPs into array

--- Monitoring/scanPorts.sh
+++ Monitoring/scanPorts.sh
@@ -1,4 +1,14 @@
 #! /bin/zsh
 #
 #Usage:
-#zsh scanPorts.sh <output file>
+#zsh scanPorts.sh < <input: CIDR> <output file> 
+
+HOSTS=$(nmap -sn $1 | grep -E -o "([1-9]{1,3}\.){3}[1-9]{1,3}")
+
+while read -r HOST; do
+    HOSTARRAY+=($HOST)
+done <<< $HOSTS
+
+for host in $HOSTARRAY; do
+    echo $host
+done
