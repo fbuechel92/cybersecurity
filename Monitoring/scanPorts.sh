d630ccb11399e087d910dfc2506fedd4269c3337
fbuechel92 <fabianbuecheler@gmx.de>
2025-01-12 22:16:55 +0100

    Finish scan function

--- Monitoring/scanPorts.sh
+++ Monitoring/scanPorts.sh
@@ -5,9 +5,16 @@
 
 # Define scan function
 scan(){
-
+    host=$HOST
+    printf '%s' $host
+    
+    for ((port=1;port<1024;port++)); do
+        echo > /dev/null 2>&1 < nc -z -w1 ${host} ${port}
+        if (( $? == 0))
+            then printf '%d' $port
+        fi
+    done
 }
-
 
 # Create a list of IPs of our network
 HOSTS=$(nmap -sn $1 | grep -E -o "([1-9]{1,3}\.){3}[1-9]{1,3}")
