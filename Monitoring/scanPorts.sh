1f5886904f6794e7add3b2f53c3ae36db1573058
fbuechel92 <fabianbuecheler@gmx.de>
2025-01-12 20:10:14 +0100

    Remove redirection

--- Monitoring/scanPorts.sh
+++ Monitoring/scanPorts.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 #
 #Usage:
-#zsh scanPorts.sh < <input: CIDR> <output file> 
+#zsh scanPorts.sh <input: CIDR> <output file> 
 
 HOSTS=$(nmap -sn $1 | grep -E -o "([1-9]{1,3}\.){3}[1-9]{1,3}")
 
