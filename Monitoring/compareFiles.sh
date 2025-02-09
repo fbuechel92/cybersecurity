62f1779d0a4e350fe4182f9a9590b9b1ae0726a8
fbuechel92 <fabianbuecheler@gmx.de>
2025-02-09 11:21:07 +0100

    Add variables and continue part

--- Monitoring/compareFiles.sh
+++ Monitoring/compareFiles.sh
@@ -2,12 +2,23 @@
 #
 #Usage:
 #Compares two input files and returns which ports are newly opened/closed
-#zsh compareFiles.sh <scanfile1>
+#zsh compareFiles.sh <scanfile1> <scanfile2>
 
 while true 
 do
 
     read LINE1 <&5 || break
     read LINE2 <&6 || break
+
+    #if lines are the same, continue with next iteration
+    if [[ $LINE1 == $LINE2 ]] 
+        THEN continue
+    fi
+
+    #Parse values to variables
+    HOST1={$LINE1%% *}
+    PORTS1=({$LINE1#* })
+    HOST2={$LINE2%% *}
+    PORTS2=({$LINE2#* })
 
 done 5< ${1:-scan_2025_02_08} 6< ${2:-scan_2025_02_09}
