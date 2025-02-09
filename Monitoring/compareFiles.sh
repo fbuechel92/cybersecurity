1d9cac81e9e1752a60aafc29da326a2d9d2fea48
fbuechel92 <fabianbuecheler@gmx.de>
2025-02-09 10:35:16 +0100

    Reading lines from different decriptors

--- Monitoring/compareFiles.sh
+++ Monitoring/compareFiles.sh
@@ -2,6 +2,11 @@
 #
 #Usage:
 #Compares two input files and returns which ports are newly opened/closed
-#zsh compareFiles.sh <scanfile1> <scanfile2>
+#zsh compareFiles.sh <scanfile1>
+
+while read -r LINE <&6
+do
+    printf $LINE
+done 5<$1 6<$2
 
 
