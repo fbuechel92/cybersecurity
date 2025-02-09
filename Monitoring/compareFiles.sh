3b5dc298cab94a8f5983434756d1d11fe970a508
fbuechel92 <fabianbuecheler@gmx.de>
2025-02-09 10:48:30 +0100

    Add EOF break

--- Monitoring/compareFiles.sh
+++ Monitoring/compareFiles.sh
@@ -4,9 +4,10 @@
 #Compares two input files and returns which ports are newly opened/closed
 #zsh compareFiles.sh <scanfile1>
 
-while read -r LINE <&6
+while true 
 do
-    printf $LINE
-done 5<$1 6<$2
 
+    read LINE1 <&5 || break
+    read LINE2 <&6 || break
 
+done 5< ${1:-scan_2025_02_08} 6< ${2:-scan_2025_02_09}
