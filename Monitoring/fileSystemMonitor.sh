635697e7a29619d9a45160b6db68eb4586b5cf03
fbuechel92 <fabianbuecheler@gmx.de>
2025-02-11 20:38:03 +0100

    Produce a baseline file

--- Monitoring/fileSystemMonitor.sh
+++ Monitoring/fileSystemMonitor.sh
@@ -3,3 +3,11 @@
 #Usage:
 #The file compares a baseline of the filesystem with the filesystem to identify differences
 
+function scan(){
+    find $1 -type f 2>/dev/null | xargs shasum 1>$2 2>/dev/null
+}
+
+#Produce baseline
+PATHTOSCAN="$HOME"
+OUTPUTPATH='baseline.output'
+scan $PATHTOSCAN $OUTPUTPATH
